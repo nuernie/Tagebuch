b57d36c7c6a36bd7083623e8675c549ab75b4717
nuernie <nerdhub@web.de>
2021-10-07 07:01:49 +0200

    v 0.0.0

--- App.js
+++ App.js
@@ -1,37 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{Component} from 'react';
-import { render } from 'react-dom';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React from 'react';
+import { FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
+export default class App extends React.Component {
+  state = { items: [] };
 
-export default class App extends Component{
-  state = {item:null};
-  render(){
-  return (
-    <View style={styles.container}>
-      <Text>{this.state.item || "Keine Einträge im Tagebuch"}</Text>
-      <TextInput
-      style = {styles.input}
-      placeholder = "Tagebuch Eintrag erstellern!"
-      autoCorrect = {false}
-      returnKeyType = "done"
-      onSubmitEditing = { text => this.setState({item: text.nativeEvent.text})}
-      />
-    
-    </View>
-  );
+  _addItem(text) {
+    this.setState({
+      items: [...this.state.items, { text, date: Date.now() }]
+    });
+  }
+
+  render() {
+    let content = <Text>Keine Einträge im Tagebuch</Text>;
+    if (this.state.items.length > 0) {
+      content = (
+        <FlatList
+          style={styles.list}
+          data={this.state.items}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+          keyExtractor={item => item.date.toString()}
+        />
+      );
+    }
+    return (
+      <View style={styles.container}>
+        {content}
+        <TextInput
+          style={styles.input}
+          placeholder="Tagebucheintrag erstellen"
+          returnKeyType="done"
+          onSubmitEditing={event => this._addItem(event.nativeEvent.text)}
+        />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  list: {
+    marginTop: 24
   },
   input: {
-    height : 40,
-  },
-
-
-
+    height: 40
+  }
 });
